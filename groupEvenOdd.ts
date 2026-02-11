75ee7d8347d0ffa6db62a0ce6fec0bbce0acfa2b
Adaamss <aademhamdi@gmail.com>
2026-02-11 16:47:52 +0100

    feat: add ex21 algorithm logic & update README file

---
+++ groupEvenOdd.ts
@@ -0,0 +1,16 @@
+function groupEvenOdd(arr: number[]) {
+  const result: { even: number[]; odd: number[] } = { even: [], odd: [] };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      result.even.push(arr[i]);
+    } else {
+      result.odd.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+const array = [1, 2, 3, 4, 5, 6];
+
+console.log(groupEvenOdd(array));
