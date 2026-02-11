9ff076bcaa84b6415b220546b405d1c1b1839c9b
Adaamss <aademhamdi@gmail.com>
2026-02-11 14:23:36 +0100

    fix: fix algorithm loop bug & add backward index algorithm logic & update README file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 this is the correction of exercice 9 : reverse array
+fixed a bug where using a for loop while using the pop will chnage the the length of the table and the loop will stop half way.
+solved this algorithms using 2 ways :
+
+1. using the pop method
+2. using a backward index and pushing each element in a new array
--- reverseArray.ts
+++ reverseArray.ts
@@ -1,13 +1,22 @@
 function reverseArray(arr: number[]) {
   const reversedArray = [];
 
-  for (let i = 0; i < arr.length; i++) {
+  while (arr.length > 0) {
     reversedArray.push(arr.pop());
   }
+  return reversedArray;
+}
 
+function reverseArrayBackward(arr: number[]) {
+  let reversedArray = [];
+  for (let i = arr.length-1; i >= 0; i--) {
+    reversedArray.push(arr[i]);
+  }
   return reversedArray;
 }
 
 const array = [1, 2, 3, 4];
+
+console.log(reverseArrayBackward(array));
 
 console.log(reverseArray(array));
