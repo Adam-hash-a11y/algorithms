e5bf855443525d0a82a96cbd30a68f52d4b936dc
Adaamss <aademhamdi@gmail.com>
2026-02-24 18:01:34 +0100

    fix: migrate jest.config.js module syntax

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = {
+export default {
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
