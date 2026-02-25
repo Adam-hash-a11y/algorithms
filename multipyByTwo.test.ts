9ed2c92acd8754d6802e89ea8f27bbe47b7e3306
Adaamss <aademhamdi@gmail.com>
2026-02-25 18:22:18 +0100

    refactor: improve test description for multiplyByTwo

--- multipyByTwo.test.ts
+++ multipyByTwo.test.ts
@@ -1,7 +1,7 @@
 import { multiplyByTwo } from "./multipyByTwo";
 
 describe("test function multiply by two", () => {
-  test("should return an array of multiuple two", () => {
+  test("should return an array of numbers where each number is multiplied by two", () => {
     //Given
 
     const arr = [1, 2, 3];
