45585f0d477fa162343a4c02d0b09509afa76417
Adaamss <aademhamdi@gmail.com>
2026-02-26 21:27:43 +0100

    refactor: fix sonarQube issues in removeDuplicates function

--- removeDuplicates.ts
+++ removeDuplicates.ts
@@ -1,19 +1,9 @@
-function removeDuplicates(arr:number[]):number[]{
-const array2:number[]=[]
-  for(let i=0; i<arr.length;i++){
-
-    if(array2.indexOf(arr[i]) == -1){
-
-      array2.push(arr[i])
+export function removeDuplicates(arr: number[]): number[] {
+  const array2: number[] = [];
+  for (const element of arr) {
+    if (!array2.includes(element)) {
+      array2.push(element);
     }
-
   }
-return array2
-
+  return array2;
 }
-
-
-const array=[1,2,2,3,3,2,5,5]
-
-
-console.log(removeDuplicates(array))
