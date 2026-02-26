81adaa2a0b21988cf3ed0d5be61d96dbea433e5d
Adaamss <aademhamdi@gmail.com>
2026-02-26 22:35:27 +0100

    refactor: fix sonarQube turnery operation in ispostive function

--- removeNegativeNumbers.ts
+++ removeNegativeNumbers.ts
@@ -1,10 +1,3 @@
-function isPositive(a:number):boolean{
-
-  return a>0?  true : false;
+export function isPositive(a: number): boolean {
+  return a > 0;
 }
-
-
-const array = [5,-9,-10,11]
-
-
-console.log(array.filter(isPositive))
