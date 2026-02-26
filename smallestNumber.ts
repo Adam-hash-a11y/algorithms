c2c791d5f63d6a786794d0458ac087109754dbdb
Adaamss <aademhamdi@gmail.com>
2026-02-26 21:50:54 +0100

    refactor: fix sonarQube issues in smallestNumber function

--- smallestNumber.ts
+++ smallestNumber.ts
@@ -1,13 +1,9 @@
-function smallestNumber(arr: number[]): number {
+export function smallestNumber(arr: number[]): number {
   let min = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
+  for (const element of arr) {
+    if (element < min) {
+      min = element;
     }
   }
   return min;
 }
-
-let array=[3,1,10,7]
-
-console.log(smallestNumber(array))
