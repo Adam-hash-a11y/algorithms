9b4cfe1d95ac0e534db8093b83b09ffaa0c25b65
Adaamss <aademhamdi@gmail.com>
2026-02-26 22:23:47 +0100

    refactor: fix sonarQube issues

--- countArrayElements.ts
+++ countArrayElements.ts
@@ -1,7 +1,4 @@
-function countArrayElements(arr: any[]): number {
+export function countArrayElements(arr: any[]): number {
   return arr.length;
 }
 
-const array = ["a", "b", "c", 9, { a: 5, c: 9 }];
-
-console.log(countArrayElements(array));
