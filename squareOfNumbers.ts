bcf366b07f83b33296fcfaf7d1dd6bbf23bb131d
Adaamss <aademhamdi@gmail.com>
2026-02-27 19:13:27 +0100

    refactor: fix sonarQube issues in squareNumbers fucntion

--- squareOfNumbers.ts
+++ squareOfNumbers.ts
@@ -1,9 +1,6 @@
-function squareNumbers(arr: number[]): number[] {
+export function squareNumbers(arr: number[]): number[] {
   for (let i = 0; i < arr.length; i++) {
     arr[i] *= arr[i];
   }
   return arr;
 }
-
-const array = [2, 4, 6];
-console.log(squareNumbers(array));
