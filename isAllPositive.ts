3b53c765b04c31c674af64311a8f4a1d8cb44921
Adaamss <aademhamdi@gmail.com>
2026-02-27 20:32:33 +0100

    refactor: fix sonarQube Issues in isAllPositive funbction

--- isAllPositive.ts
+++ isAllPositive.ts
@@ -1,15 +1,10 @@
-function isAllPostiveNumbers(arr:number[]):boolean{
-  let i=0;
-  while(i<arr.length){
-    console.log(i)
-    if(arr[i]<0){
-      return false
+export function isAllPostiveNumbers(arr: number[]): boolean {
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] < 0) {
+      return false;
     }
     i++;
   }
-  return true
+  return true;
 }
-
-
-const array = [1,-2,3,5,-8]
-console.log(isAllPostiveNumbers(array))
