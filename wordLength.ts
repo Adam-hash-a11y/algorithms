0c27c3b86bb01f238f16a87a15f9a558d236f822
Adaamss <aademhamdi@gmail.com>
2026-02-27 19:42:27 +0100

    refactor: fix SonarQube issues for wordlength function

--- wordLength.ts
+++ wordLength.ts
@@ -1,13 +1,11 @@
-function wordlength(arr:string[]):number[]{
-
-  let wordLengthArray=[]
-  for (let i =0; i< arr.length;i++){
-
-    wordLengthArray.push(arr[i].length)
+export function wordlength(arr: string[]): number[] {
+  let wordLengthArray: number[] = [];
+  for (const element of arr) {
+    wordLengthArray.push(element.length);
   }
-  return wordLengthArray
+  return wordLengthArray;
 }
 
-const array = ["cat","elephant"]
+const array = ["cat", "elephant"];
 
-console.log(wordlength(array))
+
