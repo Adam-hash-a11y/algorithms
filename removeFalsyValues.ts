ba3b9d5aec712d1d05b72e64583b23f260f05f66
Adaamss <aademhamdi@gmail.com>
2026-02-28 16:37:03 +0100

    refactor: fix sonarQube turnery operation on isfalsy function

--- removeFalsyValues.ts
+++ removeFalsyValues.ts
@@ -1,11 +1,7 @@
-function isFalsy(a: any): boolean {
-  if (a == "" || a == null || a == 0 || a == false || a == undefined) {
+export function isFalsy(a: any): boolean {
+  if (a == "" || a == null || a == 0 || !a || a == undefined) {
     return false;
   } else {
     return true;
   }
 }
-
-const array = [0, 1, false, 2, null, undefined];
-
-console.log(array.filter(isFalsy));
