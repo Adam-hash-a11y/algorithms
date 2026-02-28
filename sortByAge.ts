395444f66d7a8790f20f7dc288468c25b87143d0
Adaamss <aademhamdi@gmail.com>
2026-02-28 20:47:08 +0100

    refactor: remove useless console.logs and remove explicit  type annotation from temp variable

--- sortByAge.ts
+++ sortByAge.ts
@@ -1,9 +1,8 @@
-function sortObjectsByAge(arr: { age: number }[]) {
-  let temp: { age: number };
+export function sortObjectsByAge(arr: { age: number }[]): { age: number }[] {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j].age > arr[j + 1].age) {
-        temp = arr[j];
+        const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
@@ -11,7 +10,3 @@ function sortObjectsByAge(arr: { age: number }[]) {
   }
   return arr;
 }
-
-let array = [{ age: 30 }, { age: 50 }, { age: 90 }, { age: 10 }];
-
-console.log(sortObjectsByAge(array));
