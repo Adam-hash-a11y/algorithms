5a0664d46c10ac32345e9c816d94f2102bbfc8ef
Adaamss <aademhamdi@gmail.com>
2026-02-28 21:31:36 +0100

    refactor: fix sonarQube issues and typescript annotations

--- findDuplicates.ts
+++ findDuplicates.ts
@@ -1,20 +1,13 @@
-
-function findDuplicates (arr:number[]) :number[]{
-
-const  arrayOfDuplicates: number[]= []
-
-for(let i=0;i<arr.length;i++){
-
-  let item = arr[i]
-
- if(arr.indexOf(item)!== i){
-  arrayOfDuplicates.push(item)
- }
+export function findDuplicates(arr: number[]): number[] {
+  const arrayOfDuplicates: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (arr.indexOf(item) !== i) {
+      arrayOfDuplicates.push(item);
+    }
+  }
+  return arrayOfDuplicates;
 }
 
-  return arrayOfDuplicates
-}
-
-
-const array = [1,2,2,3,3]
-console.log(findDuplicates(array))
+const array: number[] = [];
+console.log(findDuplicates(array));
