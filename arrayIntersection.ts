b9d39563b016265683fd8d47b61267f1c00f1039
Adaamss <aademhamdi@gmail.com>
2026-02-28 16:54:51 +0100

    refactor: fix sonarQube issue on arrayIntersection function

--- arrayIntersection.ts
+++ arrayIntersection.ts
@@ -1,18 +1,9 @@
-function arraysIntersection(arr:number[],arr2:number[]){
-
-  const resultArray=[]
-  for(let i=0;i<arr.length;i++){
-
-    if(arr2.indexOf(arr[i])!=-1){
-      resultArray.push(arr[i])
+export function arraysIntersection(arr: number[], arr2: number[]): number[] {
+  const resultArray: number[] = [];
+  for (const element of arr) {
+    if (arr2.includes(element)) {
+      resultArray.push(element);
     }
   }
-  return resultArray
-
+  return resultArray;
 }
-
-
-const array1=[1]
-const array2 =[3,4,5,1]
-
-console.log(arraysIntersection(array1,array2))
