3a552cc9197bfd4cabcf765c62c4780dc4eac10c
Adaamss <aademhamdi@gmail.com>
2026-03-01 21:41:38 +0100

    refactor: fix SonarQube issues

--- uniqueSortedNumbers.ts
+++ uniqueSortedNumbers.ts
@@ -1,15 +1,14 @@
-function uniqueSortedNumbers(arr: number[]): number[] {
+export function uniqueSortedNumbers(arr: number[]): number[] {
   const array2: number[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (array2.indexOf(arr[i]) == -1) {
-      array2.push(arr[i]);
+  for (const element of arr) {
+    if (!array2.includes(element)) {
+      array2.push(element);
     }
   }
-  let temp = 0;
   for (let i = 0; i < array2.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
+    for (let j = 0; j < array2.length - 1 - i; j++) {
       if (array2[j] > array2[j + 1]) {
-        temp = array2[j];
+        const temp = array2[j];
         array2[j] = array2[j + 1];
         array2[j + 1] = temp;
       }
@@ -18,7 +17,3 @@ function uniqueSortedNumbers(arr: number[]): number[] {
 
   return array2;
 }
-
-const array = [9, 1, 2, 2, 3, 3, 2, 5, 5];
-
-console.log(uniqueSortedNumbers(array));
