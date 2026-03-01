7e03531c8f804cee5317647e5ece7308b9f28df0
Adaamss <aademhamdi@gmail.com>
2026-03-01 21:48:51 +0100

    refactor: fix sonarQUbe issues on histrogramRange function

--- histogramRange.ts
+++ histogramRange.ts
@@ -1,12 +1,10 @@
-function historgramRange(arr: number[]) {
-  let sortedHistrogram: { [key: string]: number } = {};
+export function historgramRange(arr: number[]): { [key: string]: number } {
+  const sortedHistrogram: { [key: string]: number } = {};
 
-  for (let i = 0; i < arr.length; i++) {
-    let range_start = Math.floor(arr[i] / 10) * 10;
+  for (const element of arr) {
+    let range_start = Math.floor(element / 10) * 10;
     let range_end = range_start + 9;
-    console.log(arr[i], range_start, range_end);
     let key: string = range_start + "-" + range_end;
-    console.log(key);
     if (sortedHistrogram[key] == undefined) {
       sortedHistrogram[key] = 1;
     } else {
@@ -15,7 +13,3 @@ function historgramRange(arr: number[]) {
   }
   return sortedHistrogram;
 }
-
-const array = [-2, -5, 9, 10, 12, 50, 33, 5, 1000];
-
-console.log(historgramRange(array));
