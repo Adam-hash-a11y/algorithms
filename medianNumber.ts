65dc034943c20c123777e63a3c5ce22951eb40d6
Adaamss <aademhamdi@gmail.com>
2026-03-05 21:36:47 +0100

    feat: add medianNumber function

---
+++ medianNumber.ts
@@ -0,0 +1,21 @@
+export function medianNumber(arr: number[]) {
+  let sum: number = 0;
+  let avg: number = 0;
+  let mostCloseToAverage = arr[0];
+  let distance = 0;
+  for (const element of arr) {
+    sum += element;
+  }
+  avg = sum / arr.length;
+  let minDistance = Math.abs(arr[0] - avg);
+  for (const element of arr) {
+    distance = Math.abs(element - avg);
+    if (distance < minDistance) {
+      minDistance = distance;
+      mostCloseToAverage = element;
+    }
+  }
+  return mostCloseToAverage;
+}
+const arr = [1, 3, 5];
+console.log(medianNumber(arr));
