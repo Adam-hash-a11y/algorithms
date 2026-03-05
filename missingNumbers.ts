0fba4db3d81a4ff5a9d6584a1f06a1c07e6c612c
Adaamss <aademhamdi@gmail.com>
2026-03-06 00:30:12 +0100

    feat: add missingNumber function

---
+++ missingNumbers.ts
@@ -0,0 +1,12 @@
+export function missingNumber(arr: number[]): number[] {
+  const resultarray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      let range = arr[i + 1] - arr[i];
+      for (let j = 0; j < range - 1; j++) {
+        resultarray.push(arr[i] + j + 1);
+      }
+    }
+  }
+  return resultarray;
+}
