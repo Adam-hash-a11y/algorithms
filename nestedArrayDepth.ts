04aef97d6b469e1568c54c697b7fe4af9cb8aa0d
Adaamss <aademhamdi@gmail.com>
2026-03-15 04:07:02 +0100

    refactor: replace union type with type alias

--- nestedArrayDepth.ts
+++ nestedArrayDepth.ts
@@ -1,4 +1,6 @@
-export function nestedArrayDepth(arr: any[], arrayDepth = 0) {
+type nestedArray = number | (number | number[])[];
+
+export function nestedArrayDepth(arr: nestedArray[], arrayDepth = 0) {
   for (const element of arr) {
     if (Array.isArray(element)) {
       return nestedArrayDepth(element, arrayDepth + 1);
