2947b5bc836c0b8d02f2cfcc19d10dc48ce26fc2
Adaamss <aademhamdi@gmail.com>
2026-03-15 04:00:05 +0100

    refactor: remove unused variable flagged by eslint

--- lastLogin.ts
+++ lastLogin.ts
@@ -1,5 +1,4 @@
 export function lastLogin(arr: { l: number }[]): { l: number }[] {
-  const resultArray: number[] = [];
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       const temp = arr[j];
