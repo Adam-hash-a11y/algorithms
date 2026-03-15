df0aebd92d1dea53951274afec0920dc8e0f33f1
Adaamss <aademhamdi@gmail.com>
2026-03-15 03:58:13 +0100

    refactor: fix eslint useless assignement error

--- avgOrderPerUser.ts
+++ avgOrderPerUser.ts
@@ -2,11 +2,10 @@ export function avgOrderUser(arr: { o: number[] }[]): number[] {
   const resultArray = [];
   for (const _ of arr) {
     let sum = 0;
-    let avg = 0;
     for (const element of _.o) {
       sum += element;
     }
-    avg = sum / _.o.length;
+    const avg = sum / _.o.length;
     resultArray.push(avg);
   }
   return resultArray;
