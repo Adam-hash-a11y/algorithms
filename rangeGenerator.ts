660e9b8cbc85ab82608cdbbc1f2759de50a6a075
Adaamss <aademhamdi@gmail.com>
2026-03-15 02:37:40 +0100

    refactor: fix eslint error

--- rangeGenerator.ts
+++ rangeGenerator.ts
@@ -1,10 +1,9 @@
 export function rangeGenerator(arr: number[]): number[] {
   const resultArray: number[] = [];
-  let range = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] + 1 < arr[i + 1]) {
       resultArray.push(arr[i]);
-      range = arr[i + 1] - arr[i];
+      const range = arr[i + 1] - arr[i];
       for (let j = 0; j < range - 1; j++) {
         resultArray.push(arr[i] + j + 1);
       }
