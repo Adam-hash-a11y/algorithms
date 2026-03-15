e2b4717fc1b154109a9ebb033dbd5aeb30c04408
Adaamss <aademhamdi@gmail.com>
2026-03-15 03:48:01 +0100

    refactor: remove usless variable asignement

--- belowAvg.ts
+++ belowAvg.ts
@@ -1,12 +1,11 @@
 export function belowAvg(arr: number[]): number[] {
   const resultArray = [];
-  let avg = 0;
   let sum = 0;
 
   for (const element of arr) {
     sum += element;
   }
-  avg = sum / arr.length;
+  const avg = sum / arr.length;
   for (const element of arr) {
     if (element > avg) {
       resultArray.push(element);
